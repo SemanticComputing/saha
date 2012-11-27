dd00ecf769b3b488ced48f45de1a37b59c789a99
amiika <amiika@43518cfe-5713-1dcd-c615-06b53fc7efa9>
2012-11-27 14:00:32 +0000

    First version

---
+++ trunk/src/main/java/fi/seco/saha3/web/control/VisuController.java
@@ -0,0 +1,50 @@
+package fi.seco.saha3.web.control;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.hp.hpl.jena.vocabulary.OWL;
+
+import fi.seco.saha3.index.ResourceIndex;
+import fi.seco.saha3.index.category.UICategories;
+import fi.seco.saha3.index.category.UICategoryNode;
+import fi.seco.saha3.model.SahaProject;
+
+/**
+ * Controller for the stats
+ * 
+ */
+public class VisuController extends ASahaController {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response, SahaProject project, Locale locale,
+			ModelAndView mav) throws Exception {
+			mav.setViewName( "forward:/app/visu.html" );
+			return mav;
+	}
+	
+    @RequestMapping("/{model}/visu.shtml")
+	public ModelAndView handleRequestSPARQL(HttpServletRequest request,
+			HttpServletResponse response, SahaProject project, Locale locale,
+			ModelAndView mav) throws Exception {
+			mav.setViewName( "forward:/app/visu.html" );
+			return mav;
+    }
+	
+	
+	
+}
